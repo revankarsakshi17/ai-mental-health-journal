9cee888dfee1e9cea7729432a43aed065afe70a5
Sakshi Revankar <revankarsakshi489@gmail.com>
2026-04-05 08:19:51 +0530

    frontend

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors";
 import connectDB from "./config/db.js";
-
 import authRoutes from "./routes/authRoutes.js";
 import journalRoutes from "./routes/journalRoutes.js";
 import aiRoutes from "./routes/aiRoutes.js";
 
-dotenv.config();
-
-connectDB();  
+connectDB();
 
 const app = express();
+
+app.use(cors({
+  origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+  credentials: true,
+}));
 
 app.use(express.json());
 
@@ -24,7 +27,4 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
