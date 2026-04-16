b1a7221d51c8425f4677d618660f433c64567acf
Sakshi Revankar <revankarsakshi489@gmail.com>
2026-04-16 13:38:36 +0530

    change

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -39,7 +39,7 @@ export const loginUser = async (req, res) => {
     const token = jwt.sign(
       { id: user._id },
       process.env.JWT_SECRET,
-      { expiresIn: "7d" }
+      { expiresIn: "30d" }
     );
 
     res.json({
